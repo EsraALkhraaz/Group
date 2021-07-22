feb0da5e1b084c36da01e92861ffc1111d84d4d4
EsraALkhraaz <87468945+EsraALkhraaz@users.noreply.github.com>
2021-07-22 14:32:43 +0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,16 @@
+// // function myFunction(){
+// //     $(document).ready(function(){
+// //         $("#selector").change(function(){
+// //             $("body").toggleClass("bg-secondry");
+// //             $("nav").toggleClass("navbar-dark bg-dark");
+// //             $(".custom-control-label").toggleClass("text-white");
+// //             $(".table-light").toggleClass("table-dark")
+// //         });
+// //     });
+// // };
+// function myFunction() {
+//     var element = document.body;
+//     element.classList.toggle("dark");
+//     $("nav").toggleClass("navbar-dark bg-dark");
+    
+//     }
